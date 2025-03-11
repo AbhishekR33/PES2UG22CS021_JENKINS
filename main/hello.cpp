3a23b88ce2c05e84984745be192b12f931d3cfc9
Abhishek Ravindra Magadum <141981179+AbhishekR33@users.noreply.github.com>
2025-03-11 10:41:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I am biggest noob srn 021"
+  cout << "I am biggest noob srn 021";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
